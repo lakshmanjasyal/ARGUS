fe58bb94dd4218d5878b82b333704d6c16fd0c32
aoi-web <trustmeidkmyself@gmail.com>
2025-02-07 04:09:49 +0530

    kuxh to kia h

--- src/app/free/page.js
+++ src/app/free/page.js
@@ -1,3 +1,17 @@
 'use client';
+import Navbar from '../../components/Navbar'
+import React from 'react';
 
-console.log("Hello");
+const page = () => {
+
+    return(<>
+    <div>
+        <h1>HELLOOO</h1>
+    </div>
+
+
+    </>
+
+    )
+}
+export default page
